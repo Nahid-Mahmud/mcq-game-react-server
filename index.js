24531148ae565c41bf3ac16b27d0db5b85e217b0
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2023-12-15 02:59:48 +0600

    / message changed

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send({ message: "Game Server is Running" });
 });
 
 app.listen(port, () => {
